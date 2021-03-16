64c631e56db81a057ba496eb2fb5c76718c089dc
Derrick-Tan-D-WEBDEV <Derrick-Tan-D-WEBDEV@users.noreply.github.com>
2021-03-16 21:25:11 +0800

    commit

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -20,7 +20,7 @@ export class APIService {
       // The response body may contain clues as to what went wrong,
       console.error(
         `Backend returned code ${error.status}, ` +
-        `body was: ${error.error}`);
+        `body was: ${error.error}`);a
     }
     // return an observable with a user-facing error message
     return throwError(
